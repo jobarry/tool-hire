0f6e43d86ee7de446c01849017210ffd4344253a
traynors22 <traynors22@gmail.com>
2015-02-28 13:30:03 +0000

    added style sheet

--- site/header.php
+++ site/header.php
@@ -5,7 +5,7 @@
 <title>Big Tool</title>
 <!-- makes sure it displays the mobile version on mobile and desktop version on desktop-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="../public/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-<link href="../public/css/styles.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>

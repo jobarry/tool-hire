eb4b4164fe4f634434c0d993c15d6968ae69cf44
traynors22 <traynors22@gmail.com>
2015-02-28 13:29:22 +0000

    added style sheet

---
+++ site/tab.php
@@ -0,0 +1,21 @@
+ <?php
+include("header.php"); 
+?>
+
+
+
+    <ul class="nav nav-tabs">
+
+        <li class="active"><a href="#">MyTools</a></li>
+
+        <li><a href="#">MyAccount</a></li>
+
+
+    </ul>
+
+
+
+
+ <?php
+ include("footer.php");
+ ?>

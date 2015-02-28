64546bd07dccdbd7bc5cc028647c1b4e7b7ed5e6
Matt <arneil.matt@gmail.com>
2015-02-28 18:38:52 +0000

    faq and search hard coded

---
+++ site/faq.php
@@ -0,0 +1,62 @@
+ <?php
+include("header.php");
+include("navBar.php")
+?>
+
+<div class="page-header">
+	<div class="container">
+		<div class="col-md-6 col-md-offset-3">	
+		<h1>FAQ</h1>
+	    <div class="panel panel-default">
+	        <div class="panel-body">
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>What is this website?</h4>
+					</div>
+		       		<div class="panel-body">
+						<p>Big Tool allows people to lend and rent tools from one another.</p>
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>Who can use this site?</h4>
+					</div>
+					<div class="panel-body">
+						<p>You can! Members of the public can rent and lend tools. Companies can also lend tools.</p>
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>How do you rent tools?</h4>
+					</div>
+					<div class="panel-body">
+						<p>You can search for a tool in different categories and by distance from a location.
+						You can then view the details of a tool and its avalability. When you have chosen a tool you can click "rent". 
+						Once the lender has agreed and payment is made, the lender will send you the tool.
+						</p>
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>I have a problem with my rental.</h4>
+					</div>
+					<div class="panel-body">
+						<p>You can file a dispute if you have a problem with the tool you are renting. 
+						This will notify an administator who will work to help resolve the situation.
+						</p>
+					</div>
+				</div>
+	        </div>
+	    </div>
+	    </div>
+	</div>
+</div>
+
+
+ <?php
+ include("footer.php");
+ ?>

19e0e35ac12fec00d10329823c443cadd9563d4d
jobarry <jobarry@tcd.ie>
2015-02-28 18:28:30 +0000

    finished main page

--- site/index.php
+++ site/index.php
@@ -1,42 +1,86 @@
-<?php
-include("header.php"); 
-include("navbar.php")
-?>
+	<?php
+			include("header.php"); 
+			include("navbar.php")
+			?>
 
-<div class="jumbotron">
-	    <div align="center">
-		    <h1>Big Tool</h1>
-		    <br>
-			<button type="button" class="btn btn-lg btn-info">Add a New Tool</button>
-		</div>
-		<br>
-			<div class="row">
-			<div class="col-xs-6 col-md-3">
-				<a href="#" class="thumbnail">
-				  <img src="img/Tool1.jpg" alt="...">
-				</a>
+			<div class="jumbotron">
+				    <div align="center">
+				 		    <h1 style="color:white">Big Tool</h1>
+					    <br>
+						<button type="button" class="btn btn-lg btn-info">Add a New Tool</button>
+					</div>
 				</div>
+					<br>
+					<div class="indexBackground">
+						<div class="row" align="center">
+						
+							<div class="col-xs-6 col-md-3">
+							<a href="#" class="thumbnail">
+							  <img src="img/Tool1.jpg" style="width:200px;height:200px" alt="...">Manuel Tools 
+							</a>
+							</div>
+							<div class="col-xs-6 col-md-3">
+							<a href="#" class="thumbnail">
+							  <img src="img/Tool2.jpg" style="width:200px;height:200px" alt="...">Power Tools 
+							</a>
+							</div>
 
-				<div class="col-xs-6 col-md-3">
-				<a href="#" class="thumbnail">
-				  <img src="img/Tool2.jpg" alt="...">
-				</a>
-				</div>
+							<div class="col-xs-6 col-md-3">
+							<a href="#" class="thumbnail">
+							  <img src="img/Tool3.jpg"  style="width:200px;height:200px" alt="...">Power Tools
+							</a>
+							</div>
 
-				<div class="col-xs-6 col-md-3">
-				<a href="#" class="thumbnail">
-				  <img src="img/Tool3.jpg" alt="...">
-				</a>
-				</div>
+						</div>
+							
+												<div class="row" align="center">
+						
+							<div class="col-xs-6 col-md-3">
+							<a href="#" class="thumbnail">
+							  <img src="img/Tool1.jpg" style="width:200px;height:200px" alt="...">Manuel Tools 
+							</a>
+							</div>
+							<div class="col-xs-6 col-md-3">
+							<a href="#" class="thumbnail">
+							  <img src="img/Tool2.jpg" style="width:200px;height:200px" alt="...">Power Tools 
+							</a>
+							</div>
 
-				<div class="col-xs-6 col-md-3">
-				<a href="#" class="thumbnail">
-				  <img src="img/Tool4.jpg" alt="...">
-				</a>
-				</div>
-			</div>
-</div>
+							<div class="col-xs-6 col-md-3">
+							<a href="#" class="thumbnail">
+							  <img src="img/Tool3.jpg"  style="width:200px;height:200px" alt="...">Power Tools
+							</a>
+							</div>
 
-<?php
-include("footer.php");
-?>
+						</div>
+											<div class="row" align="center">
+						
+							<div class="col-xs-6 col-md-3">
+							<a href="#" class="thumbnail">
+							  <img src="img/Tool1.jpg" style="width:200px;height:200px" alt="...">Manuel Tools 
+							</a>
+							</div>
+							<div class="col-xs-6 col-md-3">
+							<a href="#" class="thumbnail">
+							  <img src="img/Tool2.jpg" style="width:200px;height:200px" alt="...">Power Tools 
+							</a>
+							</div>
+
+							<div class="col-xs-6 col-md-3">
+							<a href="#" class="thumbnail">
+							  <img src="img/Tool3.jpg"  style="width:200px;height:200px" alt="...">Power Tools
+							</a>
+							</div>
+
+						</div>
+					</div>
+
+			  <footer style="color:black">
+			  
+			  <p align="center">Like us on  <a style="color:black" href="https://www.facebook.com/ringoboblove?fref=ts">
+			  Facebook</a> or  <a style="color:black" href="mailto:jobarry@tcd.ie">Email Us</a> </p>
+			</footer>
+
+			<?php
+			include("footer.php");
+			?>

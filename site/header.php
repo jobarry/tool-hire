729a2a23b6b63aef9177b52561e0c803a8d34e99
jobarry <jobarry@tcd.ie>
2015-03-02 16:01:22 +0000

    jquwey

--- site/header.php
+++ site/header.php
@@ -5,6 +5,8 @@
 <title>Big Tool</title>
 <!-- makes sure it displays the mobile version on mobile and desktop version on desktop-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script language="JavaScript" type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script language="JavaScript" type="text/javascript" src="js/jquery.js"></script>
 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 <link href="style.css" rel="stylesheet" type="text/css">
 </head>

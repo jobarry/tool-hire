ffbb39371fa03f1b4aa449dd69baa96ada874c1b
jobarry <jobarry@tcd.ie>
2015-03-02 15:03:06 +0000

    footer

--- site/footer.php
+++ site/footer.php
@@ -2,5 +2,43 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <!--will import the JavaScript lib from bootstrap-->
 <script src="js/bootstrap.js"></script>
+			  <footer style="color:black">
+<div id="footer" style="height:140px;">
+    <div class="container">
+        <div class="row">
+
+            <br>
+              <div class="col-md-4">
+                <center>
+                  <img src="https://www.facebook.com/images/fb_icon_325x325.png" class="img-circle" style="width:50px;height:50px" alt="the-brains">
+                  <br>
+                  <h4 class="footertext">Like us on Facebook</h4>
+                  <p class="footertext">Keep up to date with the latest from BigTool.<br>
+                </center>
+              </div>
+              <div class="col-md-4">
+                <center>
+                  <img src="http://png-1.findicons.com/files/icons/2794/metro_ui/256/gmail.png" class="img-circle" style="width:50px;height:50px" alt="...">
+                  <br>
+                  <h4 class="footertext">Messege us</h4>
+                  <p class="footertext">Let us know your experiences with this site.<br>
+                </center>
+              </div>
+              <div class="col-md-4">
+                <center>
+                  <img src="http://icons.iconarchive.com/icons/danleech/simple/1024/twitter-icon.png" class="img-circle" style="width:50px;height:50px" alt="...">
+                  <br>
+                  <h4 class="footertext">Follow us on Twitter</h4>
+                  <p class="footertext">Keep up to date with the latest from BigTool.<br>
+                </center>
+              </div>
+            </div>
+            <div class="row">
+            <p><center><a href="#">Terms and Conditions</a> <p class="footertext">Copyright 2015</p></center></p>
+        </div>
+    </div>
+</div>
+			  
+			</footer>
 </body>
 </html>

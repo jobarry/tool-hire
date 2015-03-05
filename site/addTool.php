e2e183b74aaac19046fe48933197acb7372ca8be
Matt <arneil.matt@gmail.com>
2015-03-05 10:59:24 +0000

    add tool tweak

--- site/addTool.php
+++ site/addTool.php
@@ -13,7 +13,13 @@ include("navBar.php");
 	        </div>
 
 	        <div class="panel-body">
-	        	<h3>Category</h3>
+	        <div class="row">
+		        <div class="col-md-6">
+		            <h3>Title</h3>
+					<input type="text" id="title" name="title" value="">
+				</div>
+				<div class="col-md-2">
+				    <h3>Category</h3>
 					<select title="Select tool category" name="category">
 						<option value="Manual Tools" selected>
 						Manual Tools</option>
@@ -34,39 +40,48 @@ include("navBar.php");
 						<option value="Safety and Ventilation">
 						Safety and Ventilation</option>
 					</select>
+				</div>
+			</div>
 
-	            <h3>Title</h3>
-				<input type="text" id="title" name="title" value="">
 
-
-		          <!-- Standar Form -->
+		          <!-- Standard Form -->
 		        <h3>Image</h3>
-		          <form action="" method="post" enctype="multipart/form-data" id="js-upload-form">
-		            <div class="form-inline">
-		              <div class="form-group">
-		                <input type="file" name="files[]" id="js-upload-files" multiple>
-		              </div>
-		              <button type="submit" class="btn btn-sm btn-primary" id="js-upload-submit">Upload files</button>
+		        <img src="img/noimage.jpg">
+		        <form action="" method="post" enctype="multipart/form-data" id="js-upload-form">
+		            <div class="row">
+			            <div class="col-md-3">
+			            	<input type="file" name="files[]" id="js-upload-files" multiple>
+			            </div>
+			        </div>
+			        <br>
+			        <div class="row">
+			            <div class="col-md-3">
+			        	    <button type="submit" class="btn btn-sm btn-primary" id="js-upload-submit">Upload files</button>
+			            </div>
 		            </div>
 		        </form>
         
 	            <h3>Description</h3>
-	        	<textarea name="description" id="description" max="10000" rows="5" cols="50"></textarea>
+	        	<textarea name="description" id="description" max="10000" rows="5" cols="60"></textarea>
 
+	            <div class="row">
+	            <div class="col-md-6">
 	            <h3>Availability</h3>
-				<div class="grid-col-6 grid-col-l-10 space-mbxs form-element">
-                	<input type="hidden" id="available" name="available">
-                	<input type="checkbox" class="checkbox-switch" value="true">
-                	<label for="available">
-	                    <span>
-	                        <span>Available</span>
-	                    </span>
-                    </label>
-        		</div>	
+	            </div>
+	            <br>
+	            <div class="col-md-3">
+		        		<button type="button" class="btn btn-lg btn-primary">Add Tool</button>
+				</div>
+				</div>
+				<div class="row">
+		            <div class="col-md-1">
+		                	<input type="checkbox" class="checkbox-switch" value="true">
+		            </div>
+		            <div class="col-md-6">
+		                <p>Available</p>
+		            </div>
+				</div>
 
-        		<h1>
-					<button type="button" class="btn btn-lg btn-primary">Add Tool</button>
-				</h1>
 			</div>
 		</div>
 		</div>

f5227926c75ba57f9ea9a07bc9676044b394ca38
Matt <arneil.matt@gmail.com>
2015-03-06 11:27:59 +0000

    something searchresults

--- site/searchResults.php
+++ site/searchResults.php
@@ -1,87 +1,87 @@
- <?php
+<?php
 include("header.php"); 
 include("navBar.php")
 ?>
 
 <div class="col-md-6 col-md-offset-3">
-	<h1>Search Results:</h1>
-	<div class="list-group">
-        <a href="#" class="list-group-item">
-        	<div class="row">
-        	<div class="col-md-5">
-          	<h4 class="list-group-item-heading">High Quality Hammer</h4>
-            <h5>Owner: Sarah D</h5>
-            <h5>Average User Rating 4.5/5</h5>
-          	<p class="list-group-item-text">This is a high quality hammer, only a year old. Excellent for...</p>
-            <p class="list-group-item-text">Distance: <strong>1 miles</strong></p>
-       	    <p class="list-group-item-text">Price: <strong>£5/day</strong></p>
-        	</div>
-        	<div class="col-md-2">
-        	<img style="width=10px" src="img/Hammer1.jpg">
-        	</div>
-        	</div>
-        	<br>
-			<h4><button type="button" class="btn btn-lg btn-primary">View</button></h4>
-        </a>
-        <a href="woodenHammer.php" class="list-group-item">
-            <div class="row">
-            	<div class="col-md-5">
-              	<h4 class="list-group-item-heading">Wooden Hammer</h4>
-                <h5>Owner: Rich L</h5>
-                <h5>Average User Rating 4/5</h5>
-                <p><strong>Title: </strong>Wooden hammer</br>
-                          <strong>Category:</strong> Manual tool</br>
-                          <strong>Tool: </strong>Hammer</br>
-                          <strong>Description:</strong> 
-                            Carbon steel head, Wooden grip.</br>
-                            <strong>Condition: </strong>Used</br>
-                            <strong>Price:</strong> £2 per day</p>
-              </div>
-        	    <div class="col-md-2">
-        	     <img src="img/Hammer2.jpg">
-        	    </div>
-        	</div>
-        	<br>
-			<h4><button type="button" class="btn btn-lg btn-primary">View</button></h4>
-        </a>
-        <a href="#" class="list-group-item">
-        	<div class="row">
-        	<div class="col-md-5">
-          	<h4 class="list-group-item-heading">Classic Hammer</h4>
-            <h5>Owner: Rachel P</h5>
-            <h5>Average User Rating 4/5</h5>
-          	<p class="list-group-item-text">The only hammer I've ever owned. It has never let me down...</p>
-          	<p class="list-group-item-text">Distance: <strong>5 miles</strong></p>
-     	    <p class="list-group-item-text">Price: <strong>£2/day</strong></p>
-          	</div>
-        	<div class="col-md-2">
-        	<img src="img/Hammer3.jpg">
-        	</div>
-        	</div>
-        	<br>
-			<h4><button type="button" class="btn btn-lg btn-primary">View</button></h4>
-        </a>
-        <a href="#" class="list-group-item">
-        	<div class="row">
-        	<div class="col-md-5">
-          	<h4 class="list-group-item-heading">Nice Hammer</h4>
-            <h5>Owner: Tom T</h5>
-            <h5>Average User Rating 3.5/5</h5>
-          	<p class="list-group-item-text">Does the job. It hammers stuff.</p>
-          	<p class="list-group-item-text">Distance: <strong>2 miles</strong></p>
-     	    <p class="list-group-item-text">Price: <strong>£1/day</strong></p>
-          	</div>
-        	<div class="col-md-2">
-        	<img src="img/Hammer4.jpg">
-        	</div>
-        	</div>
-        	<br>
-			<h4><button type="button" class="btn btn-lg btn-primary">View</button></h4>
-        </a>
-    </div>
+<h1>Search Results:</h1>
+  <div class="list-group">
+    <a href="#" class="list-group-item">
+      <div class="row">
+        <div class="col-md-5">
+          <h4 class="list-group-item-heading">High Quality Hammer</h4>
+          <h5>Owner: Sarah D</h5>
+          <h5>Average User Rating 4.5/5</h5>
+          <p class="list-group-item-text">This is a high quality hammer, only a year old. Excellent for...</p>
+          <p class="list-group-item-text">Distance: <strong>1 miles</strong></p>
+          <p class="list-group-item-text">Price: <strong>£5/day</strong></p>
+        </div>
+        <div class="col-md-2">
+          <img style="width=10px" src="img/Hammer1.jpg">
+        </div>
+      </div>
+      <br>
+      <h4><button type="button" class="btn btn-lg btn-primary">View</button></h4>
+    </a>
+    <a href="woodenHammer.php" class="list-group-item">
+      <div class="row">
+        <div class="col-md-5">
+          <h4 class="list-group-item-heading">Wooden Hammer</h4>
+          <h5>Owner: Rich L</h5>
+          <h5>Average User Rating 4/5</h5>
+          <p><strong>Title: </strong>Wooden hammer<br>
+            <strong>Category:</strong> Manual tool<br>
+            <strong>Tool: </strong>Hammer<br>
+            <strong>Description:</strong> 
+            Carbon steel head, Wooden grip.<br>
+            <strong>Condition: </strong>Used<br>
+            <strong>Price:</strong> £2 per day</p>
+        </div>
+        <div class="col-md-2">
+          <img src="img/Hammer2.jpg">
+        </div>
+      </div>
+      <br>
+      <h4><button type="button" class="btn btn-lg btn-primary">View</button></h4>
+    </a>
+    <a href="#" class="list-group-item">
+      <div class="row">
+        <div class="col-md-5">
+          <h4 class="list-group-item-heading">Classic Hammer</h4>
+          <h5>Owner: Rachel P</h5>
+          <h5>Average User Rating 4/5</h5>
+          <p class="list-group-item-text">The only hammer I've ever owned. It has never let me down...</p>
+          <p class="list-group-item-text">Distance: <strong>5 miles</strong></p>
+          <p class="list-group-item-text">Price: <strong>£2/day</strong></p>
+        </div>
+        <div class="col-md-2">
+          <img src="img/Hammer3.jpg">
+        </div>
+      </div>
+      <br>
+      <h4><button type="button" class="btn btn-lg btn-primary">View</button></h4>
+    </a>
+    <a href="#" class="list-group-item">
+      <div class="row">
+        <div class="col-md-5">
+          <h4 class="list-group-item-heading">Nice Hammer</h4>
+          <h5>Owner: Tom T</h5>
+          <h5>Average User Rating 3.5/5</h5>
+          <p class="list-group-item-text">Does the job. It hammers stuff.</p>
+          <p class="list-group-item-text">Distance: <strong>2 miles</strong></p>
+          <p class="list-group-item-text">Price: <strong>£1/day</strong></p>
+        </div>
+        <div class="col-md-2">
+          <img src="img/Hammer4.jpg">
+        </div>
+      </div>
+      <br>
+      <h4><button type="button" class="btn btn-lg btn-primary">View</button></h4>
+    </a>
+  </div>
 </div>
 
 
- <?php
- include("footer.php");
- ?>
+<?php
+include("footer.php");
+?>
